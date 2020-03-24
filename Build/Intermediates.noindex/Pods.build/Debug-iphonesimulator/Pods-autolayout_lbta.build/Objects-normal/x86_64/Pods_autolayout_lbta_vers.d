1b37d1f18044bccc232b2c41f7f6ede28d13146e
Cong Le <longchike@gmail.com>
2020-03-23 23:30:56 -0400

    Deleting Build folder

--- Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-autolayout_lbta.build/Objects-normal/x86_64/Pods_autolayout_lbta_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/congle/Desktop/SWIFT/autolayout_lbta/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-autolayout_lbta.build/DerivedSources/Pods_autolayout_lbta_vers.c
